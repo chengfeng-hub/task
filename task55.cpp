0a35df836ca00ff6c62fcf50b4ab9a53eb9a641e
chengfeng-hub <2287983826@qq.com>
2020-07-27 19:57:02 +0800

    字符中所有出现的数字前后加上符号*，其他字符保持不变

--- task55.cpp
+++ task55.cpp
@@ -4,31 +4,39 @@
 using namespace std;
 string MarkNum(string pInStr)
 {
-	int len;
 	int i=0;
-	len=pInStr.size();
-	int num[len]={0};
-	basic_string <char>::iterator str_Iter;
-	while(i<len)
-	{
-		if(pInStr[i]<='9'&&pInStr[i]>='0')
+	// basic_string <char>::iterator str_Iter;
+	
+		while(i<pInStr.length())
 		{
-			num[i]++;
-			cout<<num[i]<<endl;
-//			if(pInStr[i+1]>'9'||pInStr[i+1]<'0')
-//			{
-//				str_Iter = ( pInStr.begin( )+ i++); 
-//				pInStr.insert ( str_Iter , '*' );
-//			}
-//			else
-//			{
-//				while(pInStr[i++]<='9'&&pInStr[i++]>='0');
-//				str_Iter = ( pInStr.begin( )+ i-1); 
-//				pInStr.insert ( str_Iter , '*' );
-//			}
+
+
+				if(pInStr[i]>='0'&&pInStr[i]<='9'&&(i==0||pInStr[i-1]<'0'||pInStr[i-1]>'9')){
+				pInStr=pInStr.substr(0,i)+"*"+pInStr.substr(i);
+				i++;
+				}
+				if(pInStr[i]>='0'&&pInStr[i]<='9'&&(i+1==pInStr.length()||pInStr[i+1]<'0'||pInStr[i+1]>'9')){
+				pInStr=pInStr.substr(0,i+1)+"*"+pInStr.substr(i+1);
+				i++;
+				}
+			// if(pInStr[i]<='9'&&pInStr[i]>='0')
+			// {
+			// 	num[i]++;
+			// 	cout<<num[i]<<endl;
+	//			if(pInStr[i+1]>'9'||pInStr[i+1]<'0')
+	//			{
+	//				str_Iter = ( pInStr.begin( )+ i++); 
+	//				pInStr.insert ( str_Iter , '*' );
+	//			}
+	//			else
+	//			{
+	//				while(pInStr[i++]<='9'&&pInStr[i++]>='0');
+	//				str_Iter = ( pInStr.begin( )+ i-1); 
+	//				pInStr.insert ( str_Iter , '*' );
+	//			}
+			// }
+				i++;
 		}
-		i++;
-	}
 	
 
 	return pInStr;
@@ -36,9 +44,15 @@ string MarkNum(string pInStr)
 int main()
 {
 	string str,Object_str;
-	cin>>str;
-	Object_str=MarkNum(str);
-	cout<<Object_str;
+	// cin>>str;
+	while(cin>>str)
+	{
+		Object_str=MarkNum(str);
+		cout<<Object_str;
+		system("pause");
+	}
+	
+	
 	return 0;
 }
 
